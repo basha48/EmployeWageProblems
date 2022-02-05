431aa874068c943b03fc07f0479fe198f9976725
basha <bashamb787@gmail.com>
2022-02-05 14:37:21 +0530

    employee wage problems

---
+++ src/com/emp/wages/EmpMain.java
@@ -0,0 +1,13 @@
+package com.emp.wages;
+
+import com.emp.wages.EmployeeWages;
+
+public class EmpMain {
+
+	public static void main(String[] args) {
+		EmployeeWages emp = new EmployeeWages();
+		emp.maxHrsReached();
+
+	}
+
+}
